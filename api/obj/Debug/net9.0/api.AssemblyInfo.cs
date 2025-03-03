d6826381031a5ce94d827ef505a8908d9dbf3d41
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-03-03 14:08:50 +0200

    Comment DELETE

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c70902cb0a59f11fbcb63b276d05d02c4639ec50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac231506a23af66746f457c50256e616699f113")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
