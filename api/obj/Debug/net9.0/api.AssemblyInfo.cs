e82640b3650ebf9dfa03c5b23dedf2b11aea56dd
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-03-09 17:06:05 +0200

    User generated content

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6fc8de06a5152bd747b929fd2607af0820f429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f629cb56db026bf8fb67a1e815fee7458e22ebe")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
